19aedd4ac0499d199e4d94a6bc7ee0eae055d01c
barym <116033642+barym@users.noreply.github.com>
2025-11-01 21:34:48 +0100

    Match and link `RenderMode`

--- configure.py
+++ configure.py
@@ -2176,7 +2176,7 @@ config.libs = [
             Object(NonMatching, "Game/System/Overwrite.cpp"),
             Object(Matching, "Game/System/PauseButtonCheckerInGame.cpp"),
             Object(NonMatching, "Game/System/PlacedHiddenStarScenarioTable.cpp"),
-            Object(NonMatching, "Game/System/RenderMode.cpp"),
+            Object(Matching, "Game/System/RenderMode.cpp"),
             Object(NonMatching, "Game/System/ResourceHolder.cpp"),
             Object(Matching, "Game/System/ResourceHolderManager.cpp"),
             Object(Matching, "Game/System/ResourceInfo.cpp"),
